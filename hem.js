84728af7a29b9b7fb61227604e68fff1de73470d
Tw212 <Tewe07001@utb.vaxjo.se>
2025-04-27 17:14:34 +0200

    daddefsed

--- hem.js
+++ hem.js
@@ -10,3 +10,36 @@ function Menu(){
         a = true
     }
 }
+let b= true;
+
+function Pris1(){
+    if(b){
+        document.getElementById("text1").style.display = "block"
+        document.getElementById("text2").style.display = "none"
+        b = false
+        c = true
+        
+    }
+    else{
+        document.getElementById("text1").style.display = "none";
+        b = true
+        
+    }
+}
+
+let c = true;
+
+function Pris2(){
+    if(c){
+        document.getElementById("text2").style.display = "block"
+        document.getElementById("text1").style.display = "none"
+        c = false
+        b = true
+        
+    }
+    else{
+        document.getElementById("text2").style.display = "none"
+        c = true
+        
+    }
+}
