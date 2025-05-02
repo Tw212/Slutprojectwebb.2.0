8802fb36919b4616887710a7be4fba3c41980772
Tw212 <Tewe07001@utb.vaxjo.se>
2025-05-02 11:03:49 +0200

    fsefsf

--- hem.js
+++ hem.js
@@ -3,10 +3,21 @@ let a = true;
 function Menu(){
     if(a){
         document.getElementById("sidor").style.display = "block";
+        document.getElementById("Kundkassa").style.display = "none"
+        document.getElementById("Albin").style.display = "none"
+        document.getElementById("B-side").style.display = "none"
+        document.getElementById("texth").style.display = "none"
+        document.getElementById("Pris1").style.display = "none"
+        document.getElementById("Pris2").style.display = "none"
         a = false
     }
     else{
         document.getElementById("sidor").style.display = "none";
+        document.getElementById("Albin").style.display = "block"
+        document.getElementById("B-side").style.display = "block"
+        document.getElementById("texth").style.display = "block"
+        document.getElementById("Pris1").style.display = "block"
+        document.getElementById("Pris2").style.display = "block"
         a = true
     }
 }
@@ -66,3 +77,21 @@ function Kassa(){
         l = true;
     }
 }
+
+/*
+let o = true;
+let u = true;
+let Pris1 = 100;
+let Pris2 = 1000;
+
+function Läggtill1(){
+    if(o){
+        document.getElementById("Priskassa").innerHTML = Pris1;
+    }
+}
+
+function Läggtill2(){
+    if(u){
+        document.getElementById("Priskassa").innerHTML = Pris2;
+    }
+}*/
