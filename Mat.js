67f7730c91dbfdab2e8a5abb699f8e386eee6290
Tw212 <Tewe07001@utb.vaxjo.se>
2025-05-09 11:01:50 +0200

    sfszde

--- Mat.js
+++ Mat.js
@@ -2,3 +2,27 @@ function Go(i){
     document.getElementById(i).style.display ="flex";
     document.getElementById("Mat-tabel").style.display = "none";
 }
+
+
+
+function Return(i){
+    
+        document.getElementById(i).style.display = "none";
+        document.getElementById("Mat-tabel").style.display = "block";
+
+}
+
+let a = true;
+
+function Menu(){
+    if(a){
+        document.getElementById("sidor").style.display = "block";
+        document.getElementById("Mat-tabel").style.display = "none"
+        a = false
+    }
+    else{
+        document.getElementById("sidor").style.display = "none";
+        document.getElementById("Mat-tabel").style.display = "block"
+        a = true
+    }
+}
